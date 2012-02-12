fba318e2109550bd677ce2e41a6314f74abc0f30
mantlikf <mantlikf@gmail.com>
2012-02-12 10:18:33 +0000

    Update issue 32 South America and Africa splitted to the northern and southern part. Currently default settings define 12 regions instead of 10.

--- Osm2garminGUI/src/main/java/org/mantlik/osm2garminspi/MainWindowTopComponent.java
+++ Osm2garminGUI/src/main/java/org/mantlik/osm2garminspi/MainWindowTopComponent.java
@@ -25,7 +25,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
